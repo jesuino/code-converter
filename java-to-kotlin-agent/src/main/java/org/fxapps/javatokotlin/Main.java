5fe18e498a6333179318adf9a6154469ed9204ad
William Siqueira <william.fatecsjc@gmail.com>
2024-09-04 17:38:00 -0300

    Small improvements

--- java-to-kotlin-agent/src/main/java/org/fxapps/javatokotlin/Main.java
+++ java-to-kotlin-agent/src/main/java/org/fxapps/javatokotlin/Main.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.fxapps.javatokotlin.service.JavaToKotlinService;
+import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -11,27 +12,29 @@ import jakarta.inject.Inject;
 
 @QuarkusMain
 public class Main implements QuarkusApplication {
-	
+
 	@Inject
 	JavaToKotlinService javaToKotlinService;
+
+	private static final Logger log = Logger.getLogger(Main.class);
 
 	@Override
 	public int run(String... args) throws Exception {
 		if (args.length > 0) {
-			var path = Paths.get(args[0]);			
+			var path = Paths.get(args[0]);
 			if (path.toFile().exists()) {
 				var javaContent = Files.readString(path);
-				System.out.println("Converting java file to kotlin...");
+				log.info("Converting java file to kotlin...");
 				var kotlinContent = javaToKotlinService.convertJavaToKotlin(javaContent);
 				System.out.println(kotlinContent);
 			} else {
-				System.out.println("Can't open file path: " + path);
+				log.error("Can't open file path: " + path);
 			}
 		} else {
-			System.out.println("Please provide the path to a valid Java file.");
+			log.warn("Please provide the path to a valid Java file.");
 		}
-		
+
 		return 0;
 	}
-	
+
 }
